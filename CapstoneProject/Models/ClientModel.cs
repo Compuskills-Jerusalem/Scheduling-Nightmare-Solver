b08b881986a53319987568a5fcea9403175eac4c
MindyAdler <47183969+MindyAdler@users.noreply.github.com>
2019-07-24 16:40:56 +0300

    DELETE

--- CapstoneProject/Models/ClientModel.cs
+++ CapstoneProject/Models/ClientModel.cs
@@ -7,10 +7,6 @@ namespace CapstoneProject.Models
 {
     public class ClientModel
     {
-
-        //Hi Mindy!!!!
-
-        //MINDYS COMMENT 8e26ff9986b59a6f5951651578563b8432a5d370
         public int Id { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
