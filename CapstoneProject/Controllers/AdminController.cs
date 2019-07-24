f1f00b11cce391cb2a9e6061f326c47e8f886f35
Mirel Springer <mirel.spr@gmail.com>
2019-07-24 17:55:40 +0300

    Add admin controller and index view

--- CapstoneProject/Controllers/AdminController.cs
+++ CapstoneProject/Controllers/AdminController.cs
@@ -8,10 +8,82 @@ namespace CapstoneProject.Controllers
 {
     public class AdminController : Controller
     {
-        // GET: Admin
+        // GET: Admin2
         public ActionResult Index()
         {
             return View();
+        }
+
+        // GET: Admin2/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Admin2/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Admin2/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Admin2/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Admin2/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Admin2/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Admin2/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
         }
     }
 }
