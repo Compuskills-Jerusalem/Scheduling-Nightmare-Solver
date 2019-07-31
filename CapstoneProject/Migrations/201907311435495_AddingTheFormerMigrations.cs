b80b6dfe30312ab5220eae7b4af9866bf4d9775d
ViviL <47183724+aLeibowitz@users.noreply.github.com>
2019-07-31 17:37:47 +0300

    fixing migrations issue

---
+++ CapstoneProject/Migrations/201907311435495_AddingTheFormerMigrations.cs
@@ -0,0 +1,17 @@
+namespace CapstoneProject.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class AddingTheFormerMigrations : DbMigration
+    {
+        public override void Up()
+        {
+            
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
