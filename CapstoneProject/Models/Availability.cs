03298249e334713afa1ea485fd9d545dda2af8d7
rochel0108 <47180665+rochel0108@users.noreply.github.com>
2019-07-31 16:08:36 +0300

    added availability model

---
+++ CapstoneProject/Models/Availability.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CapstoneProject.Models
+{
+    public class Availability
+    {
+        public int Id { get; set; }
+        public DateTime StartTime { get; set; }
+
+    }
+}
