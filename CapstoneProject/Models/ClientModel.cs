fcf5f686b7a018ea470e3011b5f87ce550977727
ViviL <47183724+aLeibowitz@users.noreply.github.com>
2019-08-04 15:49:25 +0300

    Updated Create Client View to reflect changes in Address format

--- CapstoneProject/Models/ClientModel.cs
+++ CapstoneProject/Models/ClientModel.cs
@@ -108,11 +108,13 @@ namespace CapstoneProject.Models
         //[Required]
         [Display(Name = "Last Name")]
         public string LastName { get; set; }
+
         public string HouseNumber { get; set; }
         public string StreetName { get; set; }
         public string Town { get; set; }
         public string PostalCode { get; set; }
         public string Country { get; set; }
+
         public string Email { get; set; }
 
         public DateTime AvailableFrom { get; set; }
