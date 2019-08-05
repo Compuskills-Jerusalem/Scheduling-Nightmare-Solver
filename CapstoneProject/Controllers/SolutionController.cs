9ac1d977e8b0739afcc3460b03ff16d45152d6b6
rochel0108 <47180665+rochel0108@users.noreply.github.com>
2019-08-05 18:03:49 +0300

    moreSQL testing

--- CapstoneProject/Controllers/SolutionController.cs
+++ CapstoneProject/Controllers/SolutionController.cs
@@ -58,9 +58,34 @@ namespace CapstoneProject.Controllers
             //                   select new AvailabilityVM { ClientVm = cl, AvailableSlotVm = avsl };
             //return View(joinedTables);
 
-            var selectWithWhere = from c in _context.Clients where c.AvailableFrom == c.AvailableFrom select c;
+            //var selectWithWhere = from c in _context.Clients where c.AvailableFrom == c.AvailableFrom select c;
 
-            return View(selectWithWhere);
+            return View(GetClientsPerDay());
         }
+
+        public IEnumerable<Client> GetClientsPerDay ()
+        {
+            string query = "SELECT c1.AvailableFrom, c1.FirstName + ' ' + c1.LastName client_1, c2.FirstName + ' ' + c2.LastName client_2 FROM Clients c1 INNER JOIN Clients c2 ON c1.Id > c2.Id AND c1.GroupNameId = c2.AvailableFrom";
+            IEnumerable<Client> data = _context.Database.SqlQuery<Client>(query);
+
+            return (data);
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     }
 }
