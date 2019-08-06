550bef0bdd278f418baccb0e61841c51ccc93fe4
ViviL <47183724+aLeibowitz@users.noreply.github.com>
2019-08-06 16:41:30 +0300

    changed look of dropdown list

--- CapstoneProject/Controllers/SolutionController.cs
+++ CapstoneProject/Controllers/SolutionController.cs
@@ -1,92 +1,92 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
-using CapstoneProject.Models;
-using CapstoneProject.ViewModels;
-using System.Data;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Web;
+//using System.Web.Mvc;
+//using CapstoneProject.Models;
+//using CapstoneProject.ViewModels;
+//using System.Data;
 
-namespace CapstoneProject.Controllers
-{
-    public class SolutionController : Controller
-    {
-        private ApplicationDbContext _context;
+//namespace CapstoneProject.Controllers
+//{
+//    public class SolutionController : Controller
+//    {
+//        private ApplicationDbContext _context;
 
-        public SolutionController()
-        {
-            _context = new ApplicationDbContext();
-        }
+//        public SolutionController()
+//        {
+//            _context = new ApplicationDbContext();
+//        }
 
-        protected override void Dispose(bool disposing)
-        {
-            _context.Dispose();
-        }
+//        protected override void Dispose(bool disposing)
+//        {
+//            _context.Dispose();
+//        }
 
-        // GET: Solution
-        public ActionResult Index()
-        {
-            return View();
-        }
-        public ActionResult Test()
-        {
-            return View();
-        }
+//        // GET: Solution
+//        public ActionResult Index()
+//        {
+//            return View();
+//        }
+//        public ActionResult Test()
+//        {
+//            return View();
+//        }
 
-        public ActionResult Solve()
-        {
-            //string query = "SELECT Id, ClientID, StartTime, EndTime FROM AvailableSlots";
-            //IEnumerable<AvailableSlot> data = _context.Database.SqlQuery<AvailableSlot>(query);
+//        public ActionResult Solve()
+//        {
+//            //string query = "SELECT Id, ClientID, StartTime, EndTime FROM AvailableSlots";
+//            //IEnumerable<AvailableSlot> data = _context.Database.SqlQuery<AvailableSlot>(query);
 
-            //return View(data.ToList());
+//            //return View(data.ToList());
 
-            //var query = (from cl in _context.Clients
-            //             join avsl in _context.AvailableSlots on cl.AvailableFrom equals avsl.StartTime
-            //             select new
-            //             {
-            //                 cl.FirstName,
-            //                 avsl.StartTime,
-            //                 cl.AvailableFrom
-            //             }).ToList();
+//            //var query = (from cl in _context.Clients
+//            //             join avsl in _context.AvailableSlots on cl.AvailableFrom equals avsl.StartTime
+//            //             select new
+//            //             {
+//            //                 cl.FirstName,
+//            //                 avsl.StartTime,
+//            //                 cl.AvailableFrom
+//            //             }).ToList();
 
-            //return View(query);
+//            //return View(query);
 
-            //try select:
+//            //try select:
 
 
-            var joinedTables = from cl in _context.Clients
-                               join avsl in _context.AvailableSlots on cl.AvailableFrom equals avsl.StartTime
-                               //where cl.GroupNameId != avsl.Client.GroupNameId
+//            var joinedTables = from cl in _context.Clients
+//                               join avsl in _context.AvailableSlots on cl.AvailableFrom equals avsl.StartTime
+//                               //where cl.GroupNameId != avsl.Client.GroupNameId
 
-                               select new AvailabilityVM { ClientVm = cl, AvailableSlotVm = avsl };
-            return View(joinedTables);
+//                               select new AvailabilityVM { ClientVm = cl, AvailableSlotVm = avsl };
+//            return View(joinedTables);
 
-            //var selectWithWhere = from c in _context.Clients where c.AvailableFrom == c.AvailableFrom select c;
+//            //var selectWithWhere = from c in _context.Clients where c.AvailableFrom == c.AvailableFrom select c;
 
-            //var query = (from c in _context.Clients
-            //             where c.FirstName == "Rochel"
-            //             select new
-            //             {
-            //                 c.FirstName,
-            //                 c.AvailableFrom
-            //             }).ToList();
+//            //var query = (from c in _context.Clients
+//            //             where c.FirstName == "Rochel"
+//            //             select new
+//            //             {
+//            //                 c.FirstName,
+//            //                 c.AvailableFrom
+//            //             }).ToList();
 
-            //string query = "SELECT Id, FirstName, AvailableFrom FROM Clients";
-            //IEnumerable<Client> data = _context.Database.SqlQuery<Client>(query);
-            //return View(data);
+//            //string query = "SELECT Id, FirstName, AvailableFrom FROM Clients";
+//            //IEnumerable<Client> data = _context.Database.SqlQuery<Client>(query);
+//            //return View(data);
 
-            //return View(GetClientsPerDay());
-        }
+//            //return View(GetClientsPerDay());
+//        }
 
-        //public IEnumerable<Client> GetClientsPerDay (DateTime oneDay)
-        //{
-        //    //string query = "SELECT c1.Id, c1.AvailableFrom, c1.FirstName + ' ' + c1.LastName client_1, c2.Id, c2.FirstName + ' ' + c2.LastName client_2 FROM Clients c1 INNER JOIN Clients c2 ON c1.Id > c2.Id AND c1.AvailableFrom = c2.AvailableFrom";
-        //    //string query = "SELECT * FROM Clients";
+//        //public IEnumerable<Client> GetClientsPerDay (DateTime oneDay)
+//        //{
+//        //    //string query = "SELECT c1.Id, c1.AvailableFrom, c1.FirstName + ' ' + c1.LastName client_1, c2.Id, c2.FirstName + ' ' + c2.LastName client_2 FROM Clients c1 INNER JOIN Clients c2 ON c1.Id > c2.Id AND c1.AvailableFrom = c2.AvailableFrom";
+//        //    //string query = "SELECT * FROM Clients";
             
          
 
-        //    //return(query);
-        //}
+//        //    //return(query);
+//        //}
 
 
 
@@ -104,5 +104,5 @@ namespace CapstoneProject.Controllers
 
 
 
-    }
-}
+//    }
+//}
