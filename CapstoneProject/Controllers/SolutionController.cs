1e946c8fdcb8aed44c068bb4ab6b9e8c7d462334
rochel0108 <47180665+rochel0108@users.noreply.github.com>
2019-08-06 18:08:31 +0300

    more solver code

--- CapstoneProject/Controllers/SolutionController.cs
+++ CapstoneProject/Controllers/SolutionController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -35,11 +36,6 @@ namespace CapstoneProject.Controllers
 
         public ActionResult Solve()
         {
-            //string query = "SELECT Id, ClientID, StartTime, EndTime FROM AvailableSlots";
-            //IEnumerable<AvailableSlot> data = _context.Database.SqlQuery<AvailableSlot>(query);
-
-            //return View(data.ToList());
-
             //var query = (from cl in _context.Clients
             //             join avsl in _context.AvailableSlots on cl.AvailableFrom equals avsl.StartTime
             //             select new
@@ -51,58 +47,79 @@ namespace CapstoneProject.Controllers
 
             //return View(query);
 
-            //try select:
+            //var joinedTables = from cl in _context.Clients
+            //                   join avsl in _context.AvailableSlots on cl.AvailableFrom equals avsl.StartTime
+            //                   //where cl.GroupNameId != avsl.Client.GroupNameId
 
-
-            var joinedTables = from cl in _context.Clients
-                               join avsl in _context.AvailableSlots on cl.AvailableFrom equals avsl.StartTime
-                               //where cl.GroupNameId != avsl.Client.GroupNameId
-
-                               select new AvailabilityVM { ClientVm = cl, AvailableSlotVm = avsl };
-            return View(joinedTables);
+            //                   select new AvailabilityVM { ClientVm = cl, AvailableSlotVm = avsl };
+            //return View(joinedTables);
 
             //var selectWithWhere = from c in _context.Clients where c.AvailableFrom == c.AvailableFrom select c;
 
-            //var query = (from c in _context.Clients
-            //             where c.FirstName == "Rochel"
-            //             select new
-            //             {
-            //                 c.FirstName,
-            //                 c.AvailableFrom
-            //             }).ToList();
+            //var clients = from c in _context.Clients
+            //               where c.AvailableFrom > new DateTime(1984, 6, 1)
+            //               select c;
 
-            //string query = "SELECT Id, FirstName, AvailableFrom FROM Clients";
-            //IEnumerable<Client> data = _context.Database.SqlQuery<Client>(query);
-            //return View(data);
 
-            //return View(GetClientsPerDay());
+            //var clients = _context.Clients.OrderBy(s => s.AvailableFrom).ToList();
+
+            var dates = DatesToLoopThru();
+
+            List<List<Client>> ClientsPerDaySorted = new List<List<Client>> { };
+            List<List<List<Client>>> ClientsSorted = new List<List<List<Client>>> { };
+
+                foreach (DateTime date in dates)
+                {
+                    IEnumerable<Client> ClientsPerDay = GetClientsPerDay(date);
+                    List<Client> ClientsGroup1 = new List<Client> { };
+                    List<Client> ClientsGroup2 = new List<Client> { };
+
+                    foreach (Client client in ClientsPerDay)
+                    {
+                        if (client.GroupNameId == 1)
+                        {
+                            ClientsGroup1.Add(client);
+                        }
+                        if (client.GroupNameId == 2)
+                        {
+                            ClientsGroup2.Add(client);
+                        }
+                    }
+
+                    ClientsPerDaySorted.Add(ClientsGroup1);
+                    ClientsPerDaySorted.Add(ClientsGroup2);
+
+                ClientsSorted.Add(ClientsPerDaySorted);
+                }
+            
+           
+            return View(ClientsSorted);
+
         }
 
-        //public IEnumerable<Client> GetClientsPerDay (DateTime oneDay)
-        //{
-        //    //string query = "SELECT c1.Id, c1.AvailableFrom, c1.FirstName + ' ' + c1.LastName client_1, c2.Id, c2.FirstName + ' ' + c2.LastName client_2 FROM Clients c1 INNER JOIN Clients c2 ON c1.Id > c2.Id AND c1.AvailableFrom = c2.AvailableFrom";
-        //    //string query = "SELECT * FROM Clients";
-            
-         
+        public List<DateTime> DatesToLoopThru ()
+        {
+            List<DateTime> dates = new List<DateTime> { };
 
-        //    //return(query);
-        //}
+            foreach (Client client in _context.Clients)
+            {
+                if (!dates.Contains(client.AvailableFrom))
+                {
+                    dates.Add(client.AvailableFrom);
+                }
+            }
 
-
-
-
-
-
-
+            return dates;
+        }
 
 
+        public IEnumerable<Client> GetClientsPerDay (DateTime oneDay)
+        { 
+            var ClientsPerDay = from c in _context.Clients where c.AvailableFrom == oneDay select c;
+            return ClientsPerDay.ToList();
+        }
 
 
-
-
-
-
-
-
+        
     }
 }
